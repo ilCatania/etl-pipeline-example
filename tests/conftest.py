e2b6059ae391d6a45f7f8ecad560df6c912fde11
Gabriele Catania <gabriele.ctn@gmail.com>
2023-03-16 22:13:24 +0000

    add fixture to create test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,41 @@ def testfile() -> Callable[[Union[str, os.PathLike]], Path]:
         return test_files_dir / p
 
     return testfile_fn
+
+
+@pytest.fixture(scope="session")
+def create_test_data(testfile):
+    """Create some market and company return data for use during tests."""
+    from itertools import repeat
+
+    import numpy as np
+    import pandas as pd
+
+    # make randomisation deterministic
+    pd.core.common.random_state(42)
+
+    dates = pd.bdate_range("2000-01-01", pd.Timestamp.today())
+    n_companies = 5000
+    n_dates = 4000
+    index = [
+        list(
+            zip(repeat(i), np.random.choice(dates, size=n_dates, replace=False))
+        )
+        for i in range(n_companies)
+    ]
+    index = [x for y in index for x in y]
+    index = pd.MultiIndex.from_tuples(index, names=["companyid", "date"])
+    returns = np.random.normal(loc=0, scale=0.012, size=n_companies * n_dates)
+    df = pd.Series(index=index, data=returns, name="returns")
+
+    mkt_returns = np.random.normal(loc=0, scale=0.008, size=len(dates))
+    market = pd.Series(index=dates, data=mkt_returns)
+
+    testfiles_dir = Path(__file__).parent.joinpath("__files")
+    company_returns_file = testfiles_dir / "company_returns.pkl"
+    market_returns_file = testfiles_dir / "market_returns.pkl"
+    df.to_pickle(company_returns_file)
+    market.to_pickle(market_returns_file)
+    yield
+    company_returns_file.unlink()
+    market_returns_file.unlink()
