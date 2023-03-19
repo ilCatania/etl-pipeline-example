f62663fc63181c3078a1575324ffa54277a16869
Gabriele Catania <gabriele.ctn@gmail.com>
2023-03-19 21:35:50 +0000

    add method to resample companies

---
+++ src/etl_pipeline_example/pipeline.py
@@ -0,0 +1,39 @@
+from enum import Enum
+
+import pandas as pd
+from pandas.tseries.offsets import BaseOffset
+
+
+class ResampleStrategy(Enum):
+    """Strategy to adopt when resampling data."""
+
+    ZERO_FILL = "zero-fill"
+    """Fill with zeroes when returns are missing."""
+    NA_FILL = "na-fill"
+    """Fill with NAs when returns are missing."""
+
+
+def resample_company_returns(
+    company_data: pd.Series,
+    target_freq: str | BaseOffset = "B",
+    strategy: ResampleStrategy = ResampleStrategy.ZERO_FILL,
+) -> pd.Series:
+    """
+    Resample an input company data series.
+
+    The resulting series covers all the dates for the input frequency in the
+    input period. This assumes the input company data series has a multi index
+    containing companyid and date.
+
+    :param company_data:
+    :param target_freq: the target frequency to resample to.
+    :param strategy: the resample strategy, e.g. fill with zeroes where returns
+        are missing.
+    :return: the resampled returns.
+    """
+    groups = company_data.groupby(level="companyid")
+    resampled = groups.resample(rule=target_freq, level="date")
+    if strategy == ResampleStrategy.ZERO_FILL:
+        return resampled.sum()
+    elif strategy == ResampleStrategy.NA_FILL:
+        return resampled.sum(min_count=1)
---
+++ tests/unit/test_resample_companies.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from pandas._testing import assert_series_equal
+
+from etl_pipeline_example.pipeline import (
+    ResampleStrategy,
+    resample_company_returns,
+)
+
+
+def test_resample_company_single():
+    """Check that resampling returns for a single company works."""
+    company_data = pd.Series(
+        name="returns",
+        data=[0.05, -0.03, 0.02],
+        index=pd.MultiIndex.from_product(
+            [
+                [0],
+                [
+                    pd.Timestamp("2023-02-13"),
+                    pd.Timestamp("2023-02-20"),
+                    pd.Timestamp("2023-02-23"),
+                ],
+            ],
+            names=["companyid", "date"],
+        ),
+    )
+    expected = pd.Series(
+        name="returns",
+        data=[
+            0.05,
+            np.NaN,
+            np.NaN,
+            np.NaN,
+            np.NaN,
+            -0.03,
+            np.NaN,
+            np.NaN,
+            0.02,
+        ],
+        index=pd.MultiIndex.from_product(
+            [[0], pd.date_range("2023-02-13", "2023-02-23", freq="B")],
+            names=["companyid", "date"],
+        ),
+    )
+    actual = resample_company_returns(
+        company_data, target_freq="B", strategy=ResampleStrategy.NA_FILL
+    )
+    assert_series_equal(actual, expected)
+
+    expected2 = expected.fillna(0)
+    actual2 = resample_company_returns(
+        company_data, target_freq="B", strategy=ResampleStrategy.ZERO_FILL
+    )
+    assert_series_equal(actual2, expected2)
